8205d06acf83d1e8ba5375e0ac386963341d38f3
maheswar behera <bmaheswar439@gmail.com>
2023-10-04 06:29:16 +0530

    theme switch init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./context/Theme";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
 
 function App() {
+
+  const [themeMode, setThemeMode] = useState('light')
+
+  const darkTheme = ()=> {
+    setThemeMode('dark')
+  }
+
+  const lightTheme = ()=> {
+    setThemeMode('light')
+  }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="App-header">
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn/>
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
